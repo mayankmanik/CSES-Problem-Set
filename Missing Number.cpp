6044bdf2f264e6090154f3e2950e0b9ebf85fb98
Mayank Manik <mayank18jha@gmail.com>
2023-09-30 11:21:03 +0530

    Missing Number

---
+++ Missing Number.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ll long long
+#define all(x) x.begin(), x.end()
+#define NeedForSpeed ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define inf INT_MAX
+#define ninf INT_MIN
+#define mod 1000000007
+#define pb push_back
+#define vi vector<int>
+#define vvl vector<vector<long long>>
+#define pll pair<long long, long long>
+#define mpll map<long long, long long>
+#define minHeap priority_queue<int, vector<int>, greater<int>>
+#define loop(i, a, b) for (long long i = a; i < b; i++)
+#define printPair(v)   \
+    for (auto &it : v) \
+    cout << it.first << " -> " << it.second << endl
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vi v(n - 1, 0);
+    int sum = 0;
+    loop(i, 0, n - 1)
+    {
+        cin >> v[i];
+        sum += v[i];
+    }
+    int total = (n * (n + 1)) / 2;
+    cout << total - sum;
+}
+
+signed main()
+{
+    NeedForSpeed;
+    int tt = 1;
+    // cin >> tt;
+    loop(i, 0, tt) solve();
+    return 0;
+}
