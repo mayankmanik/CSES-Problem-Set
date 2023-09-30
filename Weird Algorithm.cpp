4059ae7f401774bed8856f675f07782b94ab817d
Mayank Manik <mayank18jha@gmail.com>
2023-09-30 11:16:48 +0530

    Weird Algorithm

---
+++ Weird Algorithm.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ll long long
+#define all(x) x.begin(), x.end()
+#define NeedForSpeed ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define inf INT_MAX
+#define ninf INT_MIN
+#define mod 1000000007
+#define pb push_back
+#define vi vector<int>
+#define vvl vector<vector<long long>>
+#define pll pair<long long, long long>
+#define mpll map<long long, long long>
+#define minHeap priority_queue<int, vector<int>, greater<int>>
+#define loop(i, a, b) for (long long i = a; i < b; i++)
+#define printPair(v)   \
+    for (auto &it : v) \
+    cout << it.first << " -> " << it.second << endl
+
+void solve()
+{
+    int n;
+    cin >> n;
+    while (n != 1)
+    {
+        cout << n << " ";
+        if (n & 1)
+            n = n * 3 + 1;
+        else
+            n /= 2;
+    }
+    cout << 1;
+}
+
+signed main()
+{
+    NeedForSpeed;
+    int tt = 1;
+    // cin >> tt;
+    loop(i, 0, tt) solve();
+    return 0;
+}
