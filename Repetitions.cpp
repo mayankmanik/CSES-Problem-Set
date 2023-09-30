4659256e584728e64bf304ff223b6a86e4420849
Mayank Manik <mayank18jha@gmail.com>
2023-09-30 20:36:58 +0530

    Repetitions

---
+++ Repetitions.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ll long long
+#define all(x) x.begin(), x.end()
+#define NeedForSpeed ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define inf INT_MAX
+#define ninf INT_MIN
+#define mod 1000000007
+#define pb push_back
+#define vi vector<int>
+#define vvl vector<vector<long long>>
+#define pll pair<long long, long long>
+#define mpll map<long long, long long>
+#define minHeap priority_queue<int, vector<int>, greater<int>>
+#define loop(i, a, b) for (long long i = a; i < b; i++)
+#define printPair(v)   \
+    for (auto &it : v) \
+    cout << it.first << " -> " << it.second << endl
+
+void solve()
+{
+    string s;
+    cin >> s;
+    int ans = 1, len = 1;
+    for (int i = 1; i < s.size(); i++)
+    {
+        if (s[i] == s[i - 1])
+        {
+            len++;
+        }
+        else
+        {
+            len = 1;
+        }
+        ans = max(ans, len);
+    }
+    cout << ans;
+}
+
+signed main()
+{
+    NeedForSpeed;
+    int tt = 1;
+    // cin >> tt;
+    loop(i, 0, tt) solve();
+    return 0;
+}
